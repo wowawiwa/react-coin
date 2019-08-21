da5f4cd503458b0642a51ce038a369528429cf37
Charles <charles.wautier@gmail.com>
2019-08-21 20:24:42 +0200

    Add pagination

---
+++ src/components/common/Pagination.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+import './Pagination.css'
+
+const Pagination = ({current, total, onPageChange}) => {
+  let elements = []
+
+  if (!total || !current || total < 2 || current > total) {
+    return null // TODO: check
+  }
+  
+  if (current > 1) {
+    elements.push(<button className="Pagination-button" onClick={(event) => onPageChange(1)}>1</button>)
+    elements.push(<button className="Pagination-button" onClick={(event) => onPageChange(current - 1)}>&larr;</button>)
+  }
+
+  elements.push(<button className="Pagination-button" disabled>{current}</button>)
+
+  if (current < total) {
+    elements.push(<button className="Pagination-button" onClick={(event) => onPageChange(current + 1)}>&rarr;</button>)
+    elements.push(<button className="Pagination-button" onClick={(event) => onPageChange(total)}>{total}</button>)
+  }
+
+  return <div className="Pagination">{elements}</div>
+}
+
+Pagination.propTypes = {
+  current: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+}
+
+export default Pagination
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { handleResponse } from '../../helpers'
 import { API_URL } from '../../config'
 import Table from './Table'
+import Pagination from '../common/Pagination'
 import Loading from '../common/Loading'
 
 class List extends React.Component {
@@ -17,23 +18,12 @@ class List extends React.Component {
       currencies: [],
       error: null,
     }
+
+    this.onPageChange = this.onPageChange.bind(this)
   }
 
   componentDidMount() {
-    this.setState({loading: true})
-
-    // TODO: Move URL to config
-    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
-      .then(handleResponse)
-      .then(data => {
-        this.setState({currencies: data.currencies, loading: false})
-      })
-      .catch(err => {
-        console.log("ERRRRooooo");
-        // TODO: if the networking fails, this will fail because err.errorMessage is undefined?
-        this.setState({error: err.errorMessage, loading: false})
-        console.log("ERRRRooooo rrrrrrr");
-      })
+    this.fetchCurrencies()
   }
   
   render() {
@@ -48,11 +38,32 @@ class List extends React.Component {
     if (this.state.currencies.length > 1) {
       return <div>
         <Table currencies={this.state.currencies}/>
-        {/* <Pagination current={this.state.page} total={this.state.pageTotal}/> */}
+        <Pagination current={this.state.page} total={this.state.pageTotal} onPageChange={this.onPageChange}/>
       </div>
     }
 
     return <div>No currencies loaded</div>
+  }
+
+  onPageChange(pageNum) {
+    console.log(pageNum);
+    
+    this.setState({page: pageNum}, this.fetchCurrencies)
+  }
+
+  fetchCurrencies() {
+    this.setState({loading: true})
+
+    // TODO: Move URL to config
+    fetch(`${API_URL}/cryptocurrencies?page=${this.state.page}&perPage=20`)
+    .then(handleResponse)
+    .then(data => {
+      this.setState({currencies: data.currencies, pageTotal: data.totalPages, loading: false})
+    })
+    .catch(err => {
+      // TODO: if the networking fails, this will fail because err.errorMessage is undefined?
+      this.setState({error: err.errorMessage, loading: false})
+    })
   }
 }
 
