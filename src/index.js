8d5a190ac83a552a04a8d29c5eb5305f6482fcdd
Charles <charles.wautier@gmail.com>
2019-08-22 11:15:07 +0200

    Add NotFound component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/common/Header'
 import List from './components/list/List'
+import NotFound from './components/notfound/NotFound'
 import './index.css'
 
 const App = () => {
-  return <div>
-    <Header/>
-    <h1>React Coin</h1>
-    <List/>
-  </div>
+  return <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" component={List} exact/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
 }
 
 ReactDOM.render(<App/>, document.getElementById("root"))
