27dcc3671bce144cbe687dc577328aff039c4b49
Charles <charles.wautier@gmail.com>
2019-09-16 17:26:29 +0200

    build

---
+++ precache-manifest.2cc0d5047d5a62c4f1024583dbe698aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6a5d484f2a5031ccbdcb2059cf16f8",
+    "url": "/react-coin/index.html"
+  },
+  {
+    "revision": "a71cbbe435a28e7faa1d",
+    "url": "/react-coin/static/js/2.a7d06391.chunk.js"
+  },
+  {
+    "revision": "1750389840d11327b9f6",
+    "url": "/react-coin/static/js/main.4c54a768.chunk.js"
+  },
+  {
+    "revision": "5fe77a0b86038e23c41b",
+    "url": "/react-coin/static/js/runtime~main.bfc76da1.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/react-coin/static/media/logo.5c0436d5.png"
+  }
+]);
