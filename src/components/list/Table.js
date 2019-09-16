774a340b60eef90762afb17ec4a9c9b19ffe95c5
Charles <charles.wautier@gmail.com>
2019-09-16 16:09:33 +0200

    Highlight on hover

--- src/components/list/Table.js
+++ src/components/list/Table.js
@@ -4,52 +4,6 @@ import styled from 'styled-components'
 
 import { renderChangePercent } from '../../helpersView';
 
-const TableContainer = styled.div`
-  overflow-x: auto; /* Needed for table to be responsive */
-`
-
-const StyledTable = styled.table`
-  width: 100%;
-
-  border-collapse: collapse;
-  border-spacing: 0;
-`
-
-const TableHead = styled.thead`
-  background-color: ${props => props.theme.mainContrastedBg};
-
-  tr th {
-    padding: 10px 20px;
-    color: ${props => props.theme.secTxt};
-    text-align: left;
-    font-size: 14px;
-    font-weight: 400;
-  }
-` 
-
-const TableBody = styled.tbody`
-  text-align: left;
-  background-color: ${props => props.theme.mainBg};
-
-  tr td {
-    padding: 24px 20px;
-    border-bottom: 2px solid ${props => props.theme.mainContrastedBg};
-    color: ${props => props.theme.priTxt};
-    cursor: pointer;
-  }
-`
-
-const TableRank = styled.span`
-  margin-right: 18px;
-  color: ${props => props.theme.secTxt};
-  font-size: 12px;
-`
-
-const TableDollar = styled.span`
-  margin-right: 6px;
-  color: ${props => props.theme.secTxt};
-`
-
 const Table = ({currencies, history}) => {
   return <TableContainer>
     <StyledTable>
@@ -85,5 +39,57 @@ const Table = ({currencies, history}) => {
     </StyledTable>
   </TableContainer>
 }
+
+const TableContainer = styled.div`
+  overflow-x: auto; /* Needed for table to be responsive */
+`
+
+const StyledTable = styled.table`
+  width: 100%;
+
+  border-collapse: collapse;
+  border-spacing: 0;
+`
+
+const TableHead = styled.thead`
+  background-color: ${props => props.theme.mainContrastedBg};
+
+  tr th {
+    padding: 10px 20px;
+    color: ${props => props.theme.secTxt};
+    text-align: left;
+    font-size: 14px;
+    font-weight: 400;
+  }
+` 
+
+const TableBody = styled.tbody`
+  text-align: left;
+  background-color: ${props => props.theme.mainBg};
+
+  tr td {
+    padding: 24px 20px;
+    border-bottom: 2px solid ${props => props.theme.mainContrastedBg};
+    color: ${props => props.theme.priTxt};
+    cursor: pointer;
+  }
+
+  tr {
+    :hover {
+      background-color: ${props => props.theme.mainHighlightedBg};
+    }
+  }
+`
+
+const TableRank = styled.span`
+  margin-right: 18px;
+  color: ${props => props.theme.secTxt};
+  font-size: 12px;
+`
+
+const TableDollar = styled.span`
+  margin-right: 6px;
+  color: ${props => props.theme.secTxt};
+`
 
 export default withRouter(Table)
